c6485e326daa579b65039bb95dbcda6d5ac7fa67
seobbang <seobbangway@naver.com>
2023-11-11 04:19:26 +0900

    라우터 실습

---
+++ seminar/week4/router-practice-project/src/pages/PostDetail.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+import { POST_LIST } from "../constants/POST_LIST";
+
+const PostDetail = () => {
+  // useParams 실습
+
+  return (
+    <Container>
+      <Header />
+      <PostDetailContainer>
+        {/* <img src={targetPost.imgSrc} alt={targetPost.title} />
+        <Title>{targetPost.title}</Title>
+        <Description>{targetPost.description}</Description> */}
+      </PostDetailContainer>
+    </Container>
+  );
+};
+
+export default PostDetail;
+
+const Container = styled.div`
+  width: 100vw;
+`;
+
+const PostDetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  gap: 20px;
+
+  width: 600px;
+  height: 600px;
+
+  margin: 30px auto 0;
+
+  background-color: #ffedf0;
+
+  & > img {
+    width: 350px;
+    height: 350px;
+
+    border-radius: 50px;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 40px;
+  font-weight: bold;
+`;
+const Description = styled.h2`
+  font-size: 30px;
+  font-weight: bold;
+
+  color: gray;
+`;
