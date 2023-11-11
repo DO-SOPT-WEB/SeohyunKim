9f34ae6cb0515d86045f26bec5b5d9a973e51b46
seoAreum <1971236@hansung.ac.kr>
2023-11-11 16:52:10 +0900

    feat: 4주차 실습

--- seminar/week4/api-practice-project/src/App.jsx
+++ seminar/week4/api-practice-project/src/App.jsx
@@ -1,25 +1,54 @@
 import styled from "styled-components";
 import { GlobalStyle } from "./style/GlobalStyle";
+import axios from "axios";
 
 import { useState } from "react";
 import Post from "./components/Post";
+import { useEffect } from "react";
 
 function App() {
   const [data, setData] = useState(null);
 
   // GET
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setData(data.data);
+    };
+    fetchData();
+  }, []);
 
   // POST
-  const postData = () => {};
+  const postData = () => {
+    try {
+      axios.post("https://jsonplaceholder.typicode.com/posts", {
+        title: "제목",
+        body: " 바디바디",
+        userId: 1,
+      });
+      console.log("성공");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   // postList
+  const postList = data ? (
+    data.map((item) => {
+      return <Post title={item.title} body={item.body} key={item.id} />;
+    })
+  ) : (
+    <p>Loading . . . </p>
+  );
 
   return (
     <>
       <GlobalStyle />
       <AppContainer>
         <CreateButton onClick={postData}>새로운 글 생성</CreateButton>
-        {/* {postList} */}
+        {postList}
       </AppContainer>
     </>
   );
